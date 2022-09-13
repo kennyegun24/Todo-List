cd4372b7cd199a22bb130ffa5c7b7fccf776b9a7
kennyegun24 <109461921+kennyegun24@users.noreply.github.com>
2022-09-14 00:49:57 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,75 @@
+const todoList = document.querySelector('.todoList')
+const todoInput = document.querySelector('.todoInput')
+const submitBtn = document.querySelector('.submitBtn')
+const selectCategory = document.querySelector('select')
+
+submitBtn.addEventListener('click', submitTodo)
+todoList.addEventListener('click', addRemove)
+selectCategory.addEventListener('click', select)
+
+function submitTodo(e){
+    e.preventDefault()
+
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add('todoDiv');
+    
+
+    const todoItem = document.createElement('li');
+    todoItem.innerText = todoInput.value;
+    todoItem.classList.add('todoItem');
+    todoDiv.appendChild(todoItem);
+
+    const markBtn = document.createElement('button');
+    markBtn.innerText = 'gfgfgf';
+    todoDiv.append(markBtn);
+    markBtn.classList.add('markBtn');
+
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'gfgfgf';
+    deleteBtn.classList.add('deleteBtn');
+    todoDiv.append(deleteBtn);
+
+    todoInput.value = '';
+    todoList.append(todoDiv);
+}
+
+function addRemove(e){
+    const item = e.target
+    if(item.classList[0]==='deleteBtn'){
+        const todoDel = item.parentElement;
+        todoDel.classList.add('delete');
+        todoDel.addEventListener('transitionend', function(){
+        todoDel.remove()
+        })
+    }
+    if(item.classList[0]==='markBtn'){
+        const todoDel = item.parentElement;
+        todoDel.classList.toggle('mark')
+    }
+}
+
+function select(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case 'all':
+                todo.style.display = 'flex'
+                break
+            case 'done':
+                if(todo.classList.contains('mark')){
+                    todo.style.display = 'flex'
+                }else{
+                    todo.style.display = 'none'
+                }
+                break
+            case 'unfinished':
+                if(!todo.classList.contains('mark')){
+                    todo.style.display = 'flex'
+                }else{
+                    todo.style.display = 'none'
+                }
+        }
+    })
+
+}
